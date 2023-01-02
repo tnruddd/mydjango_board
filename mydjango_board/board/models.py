c71517101267bbf3d35a5bf712d6ee475a72eff6
sookyoung <lsk@skkukdp.re.kr>
2023-01-02 23:24:45 +0900

    Initial commit

---
+++ mydjango_board/board/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Board(models.Model):
+    id = models.AutoField(primary_key=True)  # integer(auto-increment)
+    title = models.CharField(max_length=255)  # varchar(255)
+    content = models.TextField()  # Text
+    created_at = models.DateTimeField(auto_now_add=True)  # datetime
+    updated_at = models.DateTimeField(auto_now=True)  #
+
+
+class Comment(models.Model):
+    board = models.ForeignKey("Board", null=True, on_delete=models.SET_NULL)
+    content = models.CharField(max_length=255)
+
